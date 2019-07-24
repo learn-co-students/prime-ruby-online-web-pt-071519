067fd43e1370d58a4c80ceb525285d985a6e44d6
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-24 17:08:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+# first try
+# def prime?(num)
+#   num = 0
+#   while num < 0
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
+
+
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
